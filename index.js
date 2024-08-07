fdd81537db0995c7676e4d75c095bfb4340f2828
PRATHMESH-FEEDSPOT <prathmeshfeedspot@gmail.com>
2024-08-07 13:42:09 +0530

    ok

--- index.js
+++ index.js
@@ -1,126 +1,162 @@
-const h2 = document.querySelector('#h2');
+const h2 = document.querySelector('#question');
 const option1 = document.querySelector('#option1');
 const option2 = document.querySelector('#option2');
 const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
+const label1 = document.querySelector('#label1');
+const label2 = document.querySelector('#label2');
+const label3 = document.querySelector('#label3');
+const label4 = document.querySelector('#label4');
 const next = document.querySelector('#next');
-const opt = document.querySelector('.opt');
-const h1 = document.querySelector('h1');
+const clear = document.querySelector('#clear');
+const startAgain = document.querySelector('#startAgain');
+const home = document.querySelector('#home');
+const liveScore = document.querySelector('#liveScore');
+const questionNumber = document.querySelector('#questionNumber');
+const minutesDisplay = document.querySelector('#minutes');
+const secondsDisplay = document.querySelector('#seconds');
 
-
-
-
-const question = [{
-
-       que: "Which of the following is used to declare a variable in JavaScript?",
-       a: 'var',
-       b: 'let',
-       c: 'const',
-       d: 'all',
-       correct: 'all'
-
-
-},
-{
-       que: "Which function is used to display an alert box in JavaScript?",
-       a: 'alert',
-       b: 'msg',
-       c: 'popup',
-       d: 'notify',
-       correct: 'alert'
-
-}
-       ,
-{
-       que: " Which of the following is NOT a JavaScript data type?",
-       a: 'String',
-       b: 'boolean',
-       c: 'Float',
-       d: 'Int',
-       correct: 'boolean'
-
-},
-
-{
-       que: "  How do you access the first element of an array arr?",
-       a: ' arr.first()',
-       b: 'arr[0]',
-       c: 'arr.begin()',
-       d: 'arr.get(0)',
-       correct: 'arr[0]'
-},
-
-
-
-
+const questions = [
+    {
+        que: "Which of the following is used to declare a variable in JavaScript?",
+        a: 'var',
+        b: 'let',
+        c: 'const',
+        d: 'all',
+        correct: 'all'
+    },
+    {
+        que: "Which function is used to display an alert box in JavaScript?",
+        a: 'alert',
+        b: 'msg',
+        c: 'popup',
+        d: 'notify',
+        correct: 'alert'
+    },
+    {
+        que: "Which of the following is NOT a JavaScript data type?",
+        a: 'String',
+        b: 'boolean',
+        c: 'Float',
+        d: 'Int',
+        correct: 'Float'
+    },
+    {
+        que: "How do you access the first element of an array arr?",
+        a: 'arr.first()',
+        b: 'arr[0]',
+        c: 'arr.begin()',
+        d: 'arr.get(0)',
+        correct: 'arr[0]'
+    },
 ];
 
 let currentIndex = 0;
+let score = 0;
+let timerInterval;
 
-let paste = (currentIndex) => {
-
-
-       h2.innerHTML = question[currentIndex].que;
-       option1.nextElementSibling.innerHTML = question[currentIndex].a;
-       option2.nextElementSibling.innerHTML = question[currentIndex].b;
-       option3.nextElementSibling.innerHTML = question[currentIndex].c;
-       option4.nextElementSibling.innerHTML = question[currentIndex].d;
-
-       document.querySelectorAll('input[name="choice"]').forEach(input => {
-              input.checked = false;
-            });
+const updateScore = () => {
+    liveScore.textContent = `Score: ${score}`;
 }
 
-let score = 0;
+const updateQuestionNumber = () => {
+    questionNumber.textContent = `Question No. ${currentIndex + 1} of ${questions.length}`;
+}
 
-let checkAnswer = (currentIndex) => {
-      const   arr = [option1, option2, option3, option4];
+const paste = (index) => {
+    h2.innerHTML = questions[index].que;
+    label1.innerHTML = questions[index].a;
+    label2.innerHTML = questions[index].b;
+    label3.innerHTML = questions[index].c;
+    label4.innerHTML = questions[index].d;
 
-       arr.forEach(e => {
+    document.querySelectorAll('input[name="choice"]').forEach(input => {
+        input.checked = false;
+    });
 
+    updateQuestionNumber();
+}
 
-              if (e.checked) {
-                     let check = e.nextElementSibling.innerHTML;
-                     if (question[currentIndex].correct === check) {
-                            score++;
-                            console.log(score);
-                     }
-
-              }
-       });
-
+const checkAnswer = (index) => {
+    const selectedOption = document.querySelector('input[name="choice"]:checked');
+    if (selectedOption) {
+        const userAnswer = selectedOption.nextElementSibling.innerHTML;
+        if (userAnswer === questions[index].correct) {
+            score++;
+        }
+    }
+    updateScore();
 };
 
+const congratulation = () => {
+    h2.innerHTML = `You have completed the quiz. Your score is ${score}/${questions.length}.`;
+    document.querySelector('.options').style.display = 'none';
+    document.querySelector('.button-container').style.display = 'none';
+    clearInterval(timerInterval);
+};
 
+const resetQuiz = () => {
+    currentIndex = 0;
+    score = 0;
+    updateScore();
+    document.querySelector('.options').style.display = 'block';
+    document.querySelector('.button-container').style.display = 'flex';
+    paste(currentIndex);
+    resetTimer();
+};
 
+next.addEventListener('click', () => {
+    if (currentIndex < questions.length) {
+        checkAnswer(currentIndex);
+        currentIndex++;
+        if (currentIndex < questions.length) {
+            paste(currentIndex);
+        } else {
+            congratulation();
+        }
+    }
+});
 
-       paste(0);
+clear.addEventListener('click', () => {
+    document.querySelectorAll('input[name="choice"]').forEach(input => {
+        input.checked = false;
+    });
+});
 
-       congratulation= ()=>{
-              if(score<=2){
-                     h1.innerHTML="Don't worry, you can always try again!"    
-              }
-              if(2<score<=3){
-                     h1.innerHTML=" Keep practicing to improve your score!"    
-              }
-              if(score==4){
-                     h1.innerHTML="Great job!"    
-              }
-            
-              h2.innerHTML = `you have Completed the  quiz ,  score is ${score}/${question.length}`;
-              document.querySelector('.allOption').style.display = 'none';
-       }
-       next.addEventListener('click', () => {
-              if(currentIndex <question.length){
-             console.log(currentIndex);
-              checkAnswer(currentIndex);
-              currentIndex++;
-              if (currentIndex < question.length) {
-                     paste(currentIndex);
-                   } else {
-                     congratulation();
-                   }
-                 } else {
-                   congratulation();
-                 }
-       })
+startAgain.addEventListener('click', () => {
+    resetQuiz();
+});
+
+home.addEventListener('click', () => {
+    window.location.reload();
+});
+
+paste(currentIndex);
+
+const startTimer = () => {
+    let timeRemaining = 5 * 60; // 5 minutes in seconds
+
+    timerInterval = setInterval(() => {
+        const minutes = Math.floor(timeRemaining / 60);
+        const seconds = timeRemaining % 60;
+
+        minutesDisplay.textContent = minutes < 10 ? `0${minutes}` : minutes;
+        secondsDisplay.textContent = seconds < 10 ? `0${seconds}` : seconds;
+
+        if (timeRemaining > 0) {
+            timeRemaining--;
+        } else {
+            clearInterval(timerInterval);
+            congratulation();
+        }
+    }, 1000);
+};
+
+const resetTimer = () => {
+    clearInterval(timerInterval);
+    minutesDisplay.textContent = "05";
+    secondsDisplay.textContent = "00";
+    startTimer();
+};
+
+startTimer();
